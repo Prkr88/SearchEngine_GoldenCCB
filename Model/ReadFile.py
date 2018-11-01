f023e737192ec3edc881dde7ce48d51ada861026
Prkr88 <matanpr@gmail.com>
2018-11-01 14:46:20 +0200

    created send doc from file. created number handling

---
+++ Model/ReadFile.py
@@ -0,0 +1,31 @@
+import os
+import re
+from Model.Parse import parse_doc
+
+
+def getFiles():
+    counter = 0
+    for root, dirs, files in os.walk(
+            'C:\\Users\\Prkr_Xps\\Documents\\InformationSystems\\Year_C\\SearchEngine\\corpus\\corpus'):
+        for file in files:
+            ''' print(os.path.join(root, file))'''
+            file_path = os.path.join(root, file)
+            if counter == 0:
+                get_doc_from_file(file_path)
+            counter = counter + 1
+    print(counter)
+
+
+def get_doc_from_file(file_path):
+    skip_one = 0
+    with open(file_path, 'r') as file:
+        data = file.read().replace('\n', '')
+        data_list = data.split("<DOC>")
+        for doc in data_list:
+            if skip_one == 1:
+                doc = "<DOC>" + doc
+                parse_doc(doc)
+            else:
+                skip_one = 1
+
+'''getFiles()'''
